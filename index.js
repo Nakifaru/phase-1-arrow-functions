39dddfeeb098386a8fb724b4279880f1ae8ca209
Michael <michaelmwendwamumo@gmail.com>
2023-04-07 19:36:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    return 2000 / 100;    
+}
+
+const square = number => {
+    let x = 2;
+    return number * number;
+}
+
+const add = (num1,num2) => {
+    let a = 3;
+    let b = 4;
+    return num1 + num2;
+} 
